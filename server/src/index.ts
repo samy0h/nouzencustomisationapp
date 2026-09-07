ae3c7014e3187eb9b668d5df295f1bd31a289306
samy0h <psnsamy2006fifa@gmail.com>
2026-09-07 15:50:04 +0100

    Register coupon routes in server index

--- server/src/index.ts
+++ server/src/index.ts
@@ -54,6 +54,7 @@ app.use('/api/products', productRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/product-types', productTypeRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/coupons', couponRoutes);
 
 // 404 handler
 app.use((_req: Request, res: Response) => {
